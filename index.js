b5d0e7ffa66f91e524e58260fc6a488bf77efd21
ataurwd <ataurrahman24707@gmail.com>
2026-06-03 16:07:30 +0600

    update database

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5001;
 
 app.use(express.json());
 app.use(cors());
